0a3efd83d297ae72179cfcbe4c51e0ec20a730cc
day_ue <yuepengfei820@163.com>
2019-07-01 23:12:27 +0800

    尝试提交文件

---
+++ monitor/src/main/java/com/yuepengfei/TryTest.java
@@ -0,0 +1,12 @@
+package com.yuepengfei;
+
+/**
+ * @Author day_ue
+ * @Date 2019/7/1 23:09
+ * @Description TODO
+ **/
+public class TryTest {
+    public static void main(String[] args) {
+        System.out.println("只为尝试");
+    }
+}
